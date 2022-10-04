4c2b91ac2bcc04e44e1034635812c4d6caed77f5
omar saade <omarsaadeh11@gmail.com>
2022-10-04 09:53:31 +0300

    react

--- cryptofinder/src/store/Actions/fetchcart-actions.js
+++ cryptofinder/src/store/Actions/fetchcart-actions.js
@@ -19,109 +19,58 @@ export const fetchWalletData = createAsyncThunk(
 
 
 
+        //                Deep clone
         var Tb = JSON.parse(JSON.stringify(transformedData));
-        // console.log(Tb);
+
+
+        let new_obj = {}
+        for (let transaction of Tb) {
+            if (new_obj[transaction.name]) {
+
+                new_obj[transaction.name].amount += transaction.amount;
+            } else {
+                new_obj[transaction.name] = { ...transaction }
+            }
+        }
+        // console.log(new_obj);
+        let new_arr = Object.values(new_obj)
+        console.log(new_arr);
+
+
+
+
+
+        // console.log("Afterrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr");
+
+
+        // const l = [
+        //     { id: 0, amount: 3, name: 'Bitcoin', price: 19592 },
+        //     { id: 0, amount: 2, name: 'Etherum', price: 19592 },
+        //     { id: 3, amount: 1, name: 'USD Coin', price: 1 },
+        //     { id: 4, amount: 2, name: 'BNB', price: 286 },
+
+        // ];
+
+
+
+
+
         // const sum = transformedData.map((item, index) => {
-        // const T = item.price * item.amount;
-        // return T;
-        // 
+        //     const T = item.price * item.amount;
+        //     return T;
         // })
         // const totalPrice = sum.reduce((partialSum, a) => partialSum + a, 0); //41291
         // const Quantity = transformedData.length;
 
+        // (4)[{… }, {… }, {… }, {… }]
+        // { id: 0, amount: 1, name: 'Bitcoin', price: 19592 }
+        // { id: 0, amount: 1, name: 'Bitcoin', price: 19592 }
+        // { id: 1, amount: 1, name: 'Ethereum', price: 1322 }
+        // { id: 1, amount: 1, name: 'Ethereum', price: 1322 }
 
 
-        // { id: 0, amount: 1, name: 'Bitcoin', price: 19325 }
-        // { id: 0, amount: 1, name: 'Bitcoin', price: 19325 }
-        // { id: 1, amount: 1, name: 'Ethereum', price: 1324 }
-        // { id: 1, amount: 1, name: 'Ethereum', price: 1324 }
-
-        // [{ id: 0, amount: 2, name: 'Bitcoin', price: 19325 }]
-        var ourArray = [];
-        // console.log(ourArray);
-        for (var i = 0; i < Tb.length; i++) {
-            for (var j = 0; j < Tb[i].length; j++) {
-                var updatedItems = [];
-                // console.log(i);
-                // console.log(j);
-                if (Tb[i].name === Tb[j].name) {
-                    var updatedItem = {
-                        ...Tb[j],
-                        amount: Tb[j].amount++
-                    };
-                    updatedItems.push(updatedItem);
-                } else {
-                    updatedItems.push(Tb[j]);
-                }
-            }
-        }
-        console.log(updatedItems);
-        console.log(ourArray);
-
-
-
-
-
-
-        //     const existingCartItem = transformedData[existingCartItemIndex];
-        //     let updatedItems;
-        //     if (existingCartItem) {
-        //         const updatedItem = {
-        //             ...existingCartItem,
-        //             amount: existingCartItem.amount + amount
-        //         };
-        //         updatedItems = [...transformedData];
-        //         updatedItems[existingCartItemIndex] = updatedItem;
-        //     }
-        //     else {
-        //         updatedItems = transformedData.concat(action.payload);
-        //     }
-        //     return {
-        //         items: updatedItems,
-        //         totalAmount: totalPrice,`
-        //         quantities: Quantity
-        //     };
-
-        // }
-        // getInfo();
 
     }
 );
 
 
-
-
-
-
-
-
-
-
-
-//
-//
-//
-// import { createAsyncThunk } from '@reduxjs/toolkit';
-//
-// export const fetchWalletData = createAsyncThunk(
-    // 'wallet/fetchWallet',
-    // async () => {
-        // const response = await fetch(`https://crypto-44df7-default-rtdb.firebaseio.com/cart.json`);
-        // if (!response.ok) throw new Error();
-        // const data = await response.json();
-        // const transformedData = [];
-//
-        // for (const key in data) {
-            // const dato = {
-                // id: key,
-                // ...data[key],
-            // };
-//
-            // transformedData.push(dato);
-        // }
-        // console.log(transformedData);
-        // return transformedData;
-    // }
-// );
-//
-// 
